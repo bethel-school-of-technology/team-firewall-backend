db9ea97f5d54f3b17126da5bafea880a32c2e9ce
Zachary Mead <zacharymead11@gmail.com>
2021-02-05 19:38:42 -0500

    adding sequelize

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql2');
+var models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
