6b51e8c82bcd20a65bbb0ea8636e6616e5286e8a
Zachary Mead <zacharymead11@gmail.com>
2021-02-10 12:19:06 -0500

    signup added

--- express/routes/index.js
+++ express/routes/index.js
@@ -8,4 +8,31 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
+router.post('/signup', function(req, res, next) {
+  models.bankers.findOrCreate({
+    where: {
+      Email: req.body.email
+    },
+    defaults: {
+      FirstName: req.body.firstName,
+      LastName: req.body.lastName,
+      Password: req.body.password
+    }
+  }).spread(function (result, created){
+    if(req.body.password =! req.body.confirmPassword){
+      res.send('Passwords do not match.')
+    } else {
+      if (created) {
+        res.send('User created')
+      } else {
+        res.send('User already exists')
+      }
+    }
+  });
+});
+
 module.exports = router;
